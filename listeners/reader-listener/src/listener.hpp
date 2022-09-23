ccd5594abd93857952dfd8c18cae09162fbf5af9
Mao Sltr <56491329+maosltr@users.noreply.github.com>
2022-09-23 12:14:53 +0200

    Listening (#3)
    * Update readme.rst
    
    * ABC-123 domain participant listener is bugging: not recommended to add a listener to a domain participant, the publisher is the alternative
    
    * ABC-123 implementation of a publisher listener
    
    * ABC-123 reader listener implemented
    
    * ABC-123 subsctiber listener implemented
    
    * ABC-123 introduction of the init function
    
    * ABC-123 shared helloworld example restructured
    
    * ABC-123 helloworld example restructured
    
    * ABC-123 analysis of the threads and processes
    
    Co-authored-by: Marouane <marouane.chouhou@tttech-auto.com>

---
+++ listeners/reader-listener/src/listener.hpp
@@ -0,0 +1,353 @@
+#include "dds/dds.hpp"
+
+// data writer listener for a specific topic (source: \include\ddscxx\dds\pub\DataWriterListener.hpp)
+class DwListener : public virtual dds::pub::DataWriterListener<HelloWorldData::Msg>
+{
+public:
+    bool pubmatched = false;
+
+    virtual void on_offered_deadline_missed(
+        dds::pub::DataWriter<HelloWorldData::Msg> &writer,
+        const dds::core::status::OfferedDeadlineMissedStatus &status)
+    {
+        std::cout << "=== [Publisher] on_offered_deadline_missed" << std::endl;
+    }
+
+    virtual void on_offered_incompatible_qos(
+        dds::pub::DataWriter<HelloWorldData::Msg> &writer,
+        const dds::core::status::OfferedIncompatibleQosStatus &status)
+    {
+        std::cout << "=== [Publisher] on_offered_incompatible_qos" << std::endl;
+    }
+
+    virtual void on_liveliness_lost(
+        dds::pub::DataWriter<HelloWorldData::Msg> &writer,
+        const dds::core::status::LivelinessLostStatus &status)
+    {
+        std::cout << "=== [Publisher] on_liveliness_lost" << std::endl;
+    }
+
+    virtual void on_publication_matched(
+        dds::pub::DataWriter<HelloWorldData::Msg> &writer,
+        const dds::core::status::PublicationMatchedStatus &status)
+    {
+        std::cout << "=== [Publisher] on_publication_matched" << std::endl;
+        const std::string topic_name = writer.topic_description().name();
+
+        if (status.total_count_change() == 1)
+        {
+
+            std::cout << "=== [Publisher - data writer listener] subscriber joined for the topic '" << topic_name << "'" << std::endl;
+            pubmatched = true;
+        }
+        else if (status.total_count_change() == 0)
+        {
+
+            std::cout << "=== [Publisher - data writer listener] subscriber left " << std::endl;
+            pubmatched = false;
+        }
+    }
+};
+
+// publisher listener for any topic (source: include\ddscxx\dds\pub\PublisherListener.hpp)
+class PubListener : public virtual dds::pub::PublisherListener
+{
+public:
+    bool pubmatched = false;
+    virtual void on_offered_deadline_missed(
+        dds::pub::AnyDataWriter &writer,
+        const dds::core::status::OfferedDeadlineMissedStatus &status)
+    {
+        std::cout << "on_offered_deadline_missed" << std::endl;
+    }
+
+    virtual void on_offered_incompatible_qos(
+        dds::pub::AnyDataWriter &writer,
+        const dds::core::status::OfferedIncompatibleQosStatus &status)
+    {
+        std::cout << "on_offered_incompatible_qos" << std::endl;
+    }
+
+    virtual void on_liveliness_lost(
+        dds::pub::AnyDataWriter &writer,
+        const dds::core::status::LivelinessLostStatus &status)
+    {
+        std::cout << "on_liveliness_lost" << std::endl;
+    }
+
+    virtual void on_publication_matched(
+        dds::pub::AnyDataWriter &writer,
+        const dds::core::status::PublicationMatchedStatus &status)
+    {
+        std::cout << "=== [Publisher] on_publication_matched" << std::endl;
+        const std::string topic_name = writer.topic_description().name();
+
+        if (status.total_count_change() == 1)
+        {
+
+            std::cout << "=== [Publisher - publisher listener] subscriber joined for the topic '" << topic_name << "'" << std::endl;
+            pubmatched = true;
+        }
+        else if (status.total_count_change() == 0)
+        {
+
+            std::cout << "=== [Publisher - publisher listener] subscriber left " << std::endl;
+            pubmatched = false;
+        }
+    }
+};
+
+// domain participant listener for any topic (source: include\ddscxx\dds\domain\DomainParticipantListener.hpp)
+class DpListener : public virtual dds::domain::DomainParticipantListener
+{
+public:
+    bool pubmatched = false;
+    virtual void on_inconsistent_topic(
+        dds::topic::AnyTopic &topic,
+        const dds::core::status::InconsistentTopicStatus &status)
+    {
+        std::cout << "on_inconsistent_topic" << std::endl;
+    }
+
+    virtual void on_offered_deadline_missed(
+        dds::pub::AnyDataWriter &writer,
+        const dds::core::status::OfferedDeadlineMissedStatus &status)
+    {
+        std::cout << "on_offered_deadline_missed" << std::endl;
+    }
+
+    virtual void on_offered_incompatible_qos(
+        dds::pub::AnyDataWriter &writer,
+        const dds::core::status::OfferedIncompatibleQosStatus &status)
+    {
+        std::cout << "on_offered_incompatible_qos" << std::endl;
+    }
+
+    virtual void on_liveliness_lost(
+        dds::pub::AnyDataWriter &writer,
+        const dds::core::status::LivelinessLostStatus &status)
+    {
+        std::cout << "on_liveliness_lost" << std::endl;
+    }
+
+    virtual void on_publication_matched(
+        dds::pub::AnyDataWriter &writer,
+        const dds::core::status::PublicationMatchedStatus &status)
+    {
+        std::cout << "=== [Publisher] on_publication_matched" << std::endl;
+        const std::string topic_name = writer.topic_description().name();
+
+        if (status.total_count_change() == 1)
+        {
+
+            std::cout << "=== [Publisher - domain participant listener] subscriber joined for the topic '" << topic_name << "'" << std::endl;
+            pubmatched = true;
+        }
+        else if (status.total_count_change() == 0)
+        {
+
+            std::cout << "=== [Publisher - domain participant listener] subscriber left " << std::endl;
+            pubmatched = false;
+        }
+    }
+
+    virtual void on_requested_deadline_missed(
+        dds::sub::AnyDataReader &reader,
+        const dds::core::status::RequestedDeadlineMissedStatus &status)
+    {
+        std::cout << "on_requested_deadline_missed" << std::endl;
+    }
+
+    virtual void on_requested_incompatible_qos(
+        dds::sub::AnyDataReader &reader,
+        const dds::core::status::RequestedIncompatibleQosStatus &status)
+    {
+        std::cout << "on_requested_incompatible_qos" << std::endl;
+    }
+
+    virtual void on_sample_rejected(
+        dds::sub::AnyDataReader &reader,
+        const dds::core::status::SampleRejectedStatus &status)
+    {
+        std::cout << "on_sample_rejected" << std::endl;
+    }
+
+    virtual void on_liveliness_changed(
+        dds::sub::AnyDataReader &reader,
+        const dds::core::status::LivelinessChangedStatus &status)
+    {
+        std::cout << "on_liveliness_changed" << std::endl;
+    }
+
+    virtual void on_data_available(
+        dds::sub::AnyDataReader &reader)
+    {
+        std::cout << "on_data_available" << std::endl;
+    }
+
+    virtual void on_subscription_matched(
+        dds::sub::AnyDataReader &reader,
+        const dds::core::status::SubscriptionMatchedStatus &status)
+    {
+        std::cout << "on_subscription_matched" << std::endl;
+    }
+
+    virtual void on_sample_lost(
+        dds::sub::AnyDataReader &reader,
+        const dds::core::status::SampleLostStatus &status)
+    {
+        std::cout << "on_sample_lost" << std::endl;
+    }
+
+    virtual void on_data_on_readers(
+        dds::sub::Subscriber &subs)
+    {
+        std::cout << "on_data_on_readers" << std::endl;
+    }
+};
+
+// data reader listener for a specific topic (source: \include\ddscxx\dds\sub\DataReaderListener.hpp)
+class DrListener : public virtual dds::sub::DataReaderListener<HelloWorldData::Msg>
+{
+public:
+    bool submatched = false;
+    bool data_available = false;
+    virtual void on_requested_deadline_missed(
+        dds::sub::DataReader<HelloWorldData::Msg> &reader,
+        const dds::core::status::RequestedDeadlineMissedStatus &status)
+    {
+        std::cout << "on_requested_deadline_missed" << std::endl;
+    }
+
+    virtual void on_requested_incompatible_qos(
+        dds::sub::DataReader<HelloWorldData::Msg> &reader,
+        const dds::core::status::RequestedIncompatibleQosStatus &status)
+    {
+        std::cout << "on_requested_incompatible_qos" << std::endl;
+    }
+
+    virtual void on_sample_rejected(
+        dds::sub::DataReader<HelloWorldData::Msg> &reader,
+        const dds::core::status::SampleRejectedStatus &status)
+    {
+        std::cout << "on_sample_rejected" << std::endl;
+    }
+
+    virtual void on_liveliness_changed(
+        dds::sub::DataReader<HelloWorldData::Msg> &reader,
+        const dds::core::status::LivelinessChangedStatus &status)
+    {
+        std::cout << "on_liveliness_changed" << std::endl;
+    }
+
+    virtual void on_data_available(
+        dds::sub::DataReader<HelloWorldData::Msg> &reader)
+    {
+        std::cout << "=== [Subscriber] on_data_available" << std::endl;
+        data_available = true;
+    }
+
+    virtual void on_subscription_matched(
+        dds::sub::DataReader<HelloWorldData::Msg> &reader,
+        const dds::core::status::SubscriptionMatchedStatus &status)
+    {
+        std::cout << "=== [Subscriber] on_subscription_matched" << std::endl;
+        const std::string topic_name = reader.topic_description().name();
+
+        if (status.total_count_change() == 1)
+        {
+
+            std::cout << "=== [Subscriber - data reader listener] publisher joined for the topic '" << topic_name << "'" << std::endl;
+            submatched = true;
+        }
+        else if (status.total_count_change() == 0)
+        {
+
+            std::cout << "=== [Subscriber - data reader listener] publisher left " << std::endl;
+            submatched = false;
+        }
+    }
+
+    virtual void on_sample_lost(
+        dds::sub::DataReader<HelloWorldData::Msg> &reader,
+        const dds::core::status::SampleLostStatus &status)
+    {
+        std::cout << "on_sample_lost" << std::endl;
+    }
+};
+
+// subscriber listener for aany topic (source: \include\ddscxx\dds\sub\SubscriberListener.hpp)
+class SubListener : public virtual dds::sub::SubscriberListener
+{
+public:
+    bool submatched = false;
+    virtual void on_requested_deadline_missed(
+        dds::sub::AnyDataReader &reader,
+        const dds::core::status::RequestedDeadlineMissedStatus &status)
+    {
+        std::cout << "on_requested_deadline_missed" << std::endl;
+    }
+
+    virtual void on_requested_incompatible_qos(
+        dds::sub::AnyDataReader &reader,
+        const dds::core::status::RequestedIncompatibleQosStatus &status)
+    {
+        std::cout << "on_requested_incompatible_qos" << std::endl;
+    }
+
+    virtual void on_sample_rejected(
+        dds::sub::AnyDataReader &reader,
+        const dds::core::status::SampleRejectedStatus &status)
+    {
+        std::cout << "on_sample_rejected" << std::endl;
+    }
+
+    virtual void on_liveliness_changed(
+        dds::sub::AnyDataReader &reader,
+        const dds::core::status::LivelinessChangedStatus &status)
+    {
+        std::cout << "on_liveliness_changed" << std::endl;
+    }
+
+    virtual void on_data_available(
+        dds::sub::AnyDataReader &reader)
+    {
+        std::cout << "on_data_available" << std::endl;
+    }
+
+    virtual void on_subscription_matched(
+        dds::sub::AnyDataReader &reader,
+        const dds::core::status::SubscriptionMatchedStatus &status)
+    {
+        std::cout << "=== [Subscriber] on_subscription_matched" << std::endl;
+        const std::string topic_name = reader.topic_description().name();
+ 
+        std::cout << "=== [Subscriber - subscriber listener] " << status.current_count() << " publisher(s) for the topic '" << topic_name << "'" << std::endl;
+        std::thread::id this_id = std::this_thread::get_id(); 
+        std::cout << "=== [Subscriber - subscriber listener] " << this_id << std::endl;
+        std::cout << "=== [Subscriber - subscriber listener] " << getpid() << std::endl;
+       
+        // source: \include\ddscxx\dds\core\status\detail\TStatusImpl.hpp
+        if (status.current_count() != 0)
+        {
+            submatched = true;
+        }
+        else if (status.current_count() == 0)
+        {
+            submatched = false;
+        }
+    }
+
+    virtual void on_sample_lost(
+        dds::sub::AnyDataReader &reader,
+        const dds::core::status::SampleLostStatus &status)
+    {
+        std::cout << "on_sample_lost" << std::endl;
+    }
+
+    virtual void on_data_on_readers(
+        dds::sub::Subscriber &subs)
+    {
+        std::cout << "on_data_on_readers" << std::endl;
+    }
+};
---
+++ listeners/reader-listener/src/publisher2.cpp
@@ -0,0 +1,64 @@
+#include <cstdlib>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <fstream>
+#include <string>
+#include <algorithm>
+/* Include the C++ DDS API. */
+#include "dds/dds.hpp"
+
+/* Include data type and specific traits to be used with the C++ DDS API. */
+#include "Data.hpp"
+#include "init.cpp"
+
+#include "myFunctions.hpp"
+
+using namespace org::eclipse::cyclonedds;
+
+int main()
+{
+    try
+    {
+        std::cout << "=== [Publisher] Create writer." << std::endl;
+        dds::pub::DataWriter<HelloWorldData::Msg>  writer =  init_publisher();
+      
+
+        // pick a random message to publish
+
+        
+        std::fstream myFile;
+        myFile.open("../data/words.txt");
+
+        int counter = 1;
+        const int userID = 2;
+        bool publish = true;
+        while (publish && counter != 20)
+        {
+            // pick a random message to publish
+            std::string message = random_message("../data/words.txt");
+
+            /* Create a message to write. */
+            HelloWorldData::Msg msg(userID, message, counter);
+
+            /* Write the message. */
+            std::cout << "=== [Publisher] Write sample " << counter << " (" << userID << ", " << message << ")" << std::endl;
+            writer.write(msg);
+            std::thread::id this_id = std::this_thread::get_id(); 
+            std::cout << "=== [Publisher] " << this_id << std::endl;
+            std::cout << "=== [Publisher] " << getpid() << std::endl;
+
+            counter++;
+            std::this_thread::sleep_for(std::chrono::milliseconds(2500));
+        }
+    }
+
+    catch (const dds::core::Exception &e)
+    {
+        std::cerr << "=== [Publisher] Exception: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::cout << "=== [Publisher] Done." << std::endl;
+
+    return EXIT_SUCCESS;
+}
